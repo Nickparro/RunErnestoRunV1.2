8d90de3d46e35dcc0f8d4568d0602158dae6446a
Nicolas Parroquiano <nicolasparroquiano@gmail.com>
2024-02-27 18:11:25 -0500

    Enemy speed increased and optimizised assets added
    RepawnSystem can increase the enemy prefab's speed

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,13 +6,12 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     public float speed;
-    // Start is called before the first frame update
+    
     void Start()
     {
        
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
        transform.Translate(-1 * speed * Time.deltaTime,0,0);
@@ -21,4 +20,5 @@ public class EnemyController : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other) {
         Destroy(gameObject,0);
     }
+
 }
--- Assets/Scripts/RespawnSystem.cs
+++ Assets/Scripts/RespawnSystem.cs
@@ -2,32 +2,73 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using JetBrains.Annotations;
 
 public class RespawnSystem : MonoBehaviour
 {
     [SerializeField] private GameObject[] enemies;
     [SerializeField] private GameObject[] rows;
-    [SerializeField] private float enemyTime;
-    public float respawnTime;
+    [SerializeField] private float currentEnemySpeed;
+    [SerializeField] private float enemyInitialSpeed = 3f;
+    [SerializeField] private float enemyMaxSpeed = 10f;
+    [SerializeField] private float enemyIncreaseSpeed = 0.5f;
+    [SerializeField] private float intervalEnemySpeed = 10f;
+    [SerializeField] private float delayEnemySpeed = 20f;
+    [SerializeField] private float enemyRespawnTime;
+    [SerializeField] private float countRespawnTime;
+    [SerializeField] private float subtractEnemyTime = 0.5f;
+    [SerializeField] private float intervalSpawn = 10f;
+    [SerializeField] private float minIntervalSpawn = 1f;
+    [SerializeField] private float delaySpawn = 20f;
+    
     void Start()
     {
-
+        StartCoroutine(IncreaseSpeed());   
+        StartCoroutine(RaiseEnemiesCoroutine());
+        currentEnemySpeed = enemyInitialSpeed;
     }
     void Update()
     {
-        respawnTime += Time.deltaTime;
 
-        if(respawnTime >= enemyTime)
+        countRespawnTime += Time.deltaTime; 
+        
+        if(countRespawnTime >= enemyRespawnTime)
         {
-            respawnTime = 0;
-            SpawnEnemy();
+             countRespawnTime = 0;
+             SpawnEnemy();
         }
+
     }
     void SpawnEnemy()
     {
         int numberRows = Random.Range(0,rows.Length);
         int enemySelected = Random.Range(0, enemies.Length);
+        enemies[enemySelected].GetComponent<EnemyController>().speed = currentEnemySpeed;
         Vector2 enemyPosition = new Vector2(rows[numberRows].transform.position.x,rows[numberRows].transform.position.y);
+        
         Instantiate(enemies[enemySelected], enemyPosition, Quaternion.identity);
     }
+
+    IEnumerator IncreaseSpeed()
+    {
+        yield return new WaitForSeconds(delayEnemySpeed);
+        while (currentEnemySpeed < enemyMaxSpeed)
+        {
+            yield return new WaitForSeconds(intervalEnemySpeed);
+            currentEnemySpeed += enemyIncreaseSpeed;
+        }
+    }
+
+    IEnumerator RaiseEnemiesCoroutine()
+    {
+        yield return new WaitForSeconds(delaySpawn); // Delay inicial
+
+        while (enemyRespawnTime > minIntervalSpawn)
+        {
+            yield return new WaitForSeconds(intervalSpawn);
+            enemyRespawnTime -= subtractEnemyTime;
+        }
+    }
+
+
 }
